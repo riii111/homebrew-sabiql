4e2018ffca1c9bfb59399f2367ad66e126830a3d
riii111 <131587765+riii111@users.noreply.github.com>
2026-06-14 10:53:23 +0000

    sabiql: add 1.13.0 bottle.

--- Formula/sabiql.rb
+++ Formula/sabiql.rb
@@ -5,6 +5,13 @@ class Sabiql < Formula
   sha256 "277756214f6e1ec00d677503569adff9fbadcd6993190c9728443694faff9397"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/riii111/homebrew-sabiql/releases/download/sabiql-1.13.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9754ab7e237554f42cc0c2a9d2093f42204b377fe7097f54c95ac85aea9d8167"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f299ccaa3c8ce0eb32282d9dd158b65c0c244abd92fb63fdf242d4d1080f6773"
+    sha256 cellar: :any,                 x86_64_linux:  "8911a4d4309b5a5bfc790dac51dbbef11352c30c569182e851469043113b7a23"
+  end
+
   depends_on "rust" => :build
 
   def install
